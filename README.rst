43713f0bd552eafd095f83188e87c013d9c25bab
Guido A.J. Stevens <guido.stevens@cosent.nl>
2013-07-03 08:42:48 +0000

    fork from diazotheme.bootstrap

--- README.rst
+++ README.rst
@@ -1,12 +1,13 @@
 Introduction
 ============
 
-Plone theme based on Twitter Bootstrap 2.0
+Plone theme based on Twitter Bootstrap 2.0.
+Forked from diazotheme.bootstrap.
 
 Installation
 ============
 
-Install the ``diazotheme.bootstrap`` product using the Add-on Control Panel.
+Install the ``plonesocial.theme`` product using the Add-on Control Panel.
 This will activate the theme and also setup Plone's default ``public.css``
 to only included when Diazo is enabled or when you are viewing the Theming 
 Control Panel. This theme includes its own ``public.css`` which had several 
--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
-import os
 
-version = '0.4.dev0'
+version = '0.5.dev0'
 
-setup(name='diazotheme.bootstrap',
+setup(name='plonesocial.theme',
       version=version,
       description="Plone theme based on Twitter's Bootstrap CSS",
-      long_description=open("README.rst").read() + "\n" +
-                       open("HISTORY.txt").read(),
+      long_description=(open("README.rst").read() + "\n" +
+                        open("HISTORY.txt").read()),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
+      classifiers=["Programming Language :: Python", ],
       keywords='plone diazo theme',
-      author='Izhar Firdaus',
-      author_email='izhar@inigo-tech.com',
-      url='https://github.com/kagesenshi/diazotheme.bootstrap',
+      author='Guido Stevens / Based on work by Izhar Firdaus',
+      author_email='guido.stevens@cosent.net',
+      url='https://github.com/cosent/plonesocial.theme',
       license='Apache License 2.0',
       packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['diazotheme'],
+      namespace_packages=['plonesocial'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
